f9bd1a000299afb76ce0ddfe362a906fe877a461
federico barbarini <federico.barbarini@gmail.com>
2022-06-15 01:13:29 +0200

    Add project files.

---
+++ WebAPI-Sample1/Helper/Extension.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebAPI_Sample1.Helper
+{
+    public static class Extension
+    {
+
+        #region --> ToReal
+
+        public static string ToReal(this string? value)
+        {
+            if (value == null) return string.Empty;
+            return value.ToString();
+        }
+
+        public static Guid ToReal(this Guid? value)
+        {
+            if (value == null) return Guid.Empty;
+            return value.Value;
+        }
+
+        public static int ToReal(this int? value)
+        {
+            if (value == null) return 0;
+            return value.Value;
+        }
+
+        public static decimal ToReal(this decimal? value)
+        {
+            if (value == null) return 0;
+            return value.Value;
+        }
+
+        public static bool ToReal(this bool? value)
+        {
+            if (value == null) return false;
+            return value.Value;
+        }
+
+        public static DateTime ToReal(this DateTime? value)
+        {
+            if (value == null) return DateTime.MinValue;
+            return value.Value;
+        }
+
+        public static TimeSpan ToReal(this TimeSpan? value)
+        {
+            if (value == null) return TimeSpan.MinValue;
+            return value.Value;
+        }
+
+        #endregion
+
+        #region --> SwaggerJWTAuth
+
+        public static void AddSwaggerJWTAuth(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ WebAPI-Sample1/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using WebAPI_Sample1.Helper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.AddSwaggerJWTAuth();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.Run();
